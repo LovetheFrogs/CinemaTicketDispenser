cd86b9a17927241ad8ed2871fd4ac3555fe346cc
LovetheFrogs <marcosferrerzalve@gmail.com>
2022-04-15 12:19:42 +0200

    cambios en Session

--- CinemaTicketDispenser/src/vending/Session.java
+++ CinemaTicketDispenser/src/vending/Session.java
@@ -1,5 +1,17 @@
-package cinematicketdispenser;
+package vending;
+
+import auxclasses.Pair;
+import java.util.Set;
 
 public class Session {
+    private Pair hour;
+    private Set<Seat> occupiedSeatSet;
     
+    public Session(int x, int y){
+        this.hour = new Pair(x, y);
+    }
+    
+    public boolean isOccupied(int x, int y){
+        if()
+    }
 }
