ae5bef52342b74d2fe3c34aaf0f5179dc6ccf358
LovetheFrogs <marcosferrerzalve@gmail.com>
2022-04-17 13:23:20 +0200

    Implementación clase Film.
    Finalizada la implementación de la clase Film.

--- CinemaTicketDispenser/src/vending/Film.java
+++ CinemaTicketDispenser/src/vending/Film.java
@@ -1,5 +1,32 @@
 package vending;
 
-public class Film {
+public class Film implements Serializable{
     
+    private final String name;
+    private final String poster;
+    private final int duration;
+    private final String description;
+
+    public Film(String name, String poster, int duration, String description) {
+        this.name = name;
+        this.poster = poster;
+        this.duration = duration;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
