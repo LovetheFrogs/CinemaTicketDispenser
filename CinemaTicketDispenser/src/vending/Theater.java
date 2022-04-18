38491595f664e2fcff4da5f652c641af82030d2c
LovetheFrogs <marcosferrerzalve@gmail.com>
2022-04-18 11:29:28 +0200

    Clase Theater
    Implementación inicial de la clase Theater. También se ha añadido el método resetSeats a la clase Session para restablecer los sitios ocupados.

--- CinemaTicketDispenser/src/vending/Theater.java
+++ CinemaTicketDispenser/src/vending/Theater.java
@@ -1,5 +1,71 @@
-package cinematicketdispenser;
+package vending;
 
-public class Theater {
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Theater implements Serializable{
+    
+    private int number;
+    private int price;
+    private HashSet<Seat> seatSet;
+    private ArrayList<Session> sessions;
+    private Film filmList;
+    
+    public Theater(int number, int price, Film filmList){
+        this.number = number;
+        this.price = price;
+        this.seatSet = new HashSet<>();
+        this.filmList = filmList;
+    }
+    
+    public int getNumber(){
+        return number;
+    }
+    
+    public int getPrice(){
+        return price;
+    }
+    
+    public void addSession(Session session){
+        this.sessions.add(session);
+    }
+    
+    public Film getFilm(){
+        return filmList;
+    }
+    
+    public ArrayList<Session> getSessionList(){
+        return sessions;
+    }
+    
+    public HashSet<Seat> getSeatSet(){
+        return seatSet;
+    }
+    
+    public int getMaxRows(){
+        int max = 0;
+        for(Seat element: seatSet){
+            if(element.getRow() > max)
+                max = element.getRow();
+        }
+        
+        return max;
+    }
+    
+    public int getMaxCols(){
+        int max = 0;
+        for(Seat element: seatSet){
+            if(element.getCol() > max)
+                max = element.getCol();
+        }
+        
+        return max;
+    }
+    
+    public void loadSeats(){
+        for(int i = 0; i < sessions.size(); i++){
+            sessions.get(i).resetSeats();
+        }
+    }
     
 }
