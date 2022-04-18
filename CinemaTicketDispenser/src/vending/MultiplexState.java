61c1673e0f391f2bc8dfcc602e9e8bdd47e8c9df
LovetheFrogs <marcosferrerzalve@gmail.com>
2022-04-18 11:45:46 +0200

    Implementada clase MultiplexState.
    Añadido patrón singleton, ya que solo hay uno por cada máquina.

--- CinemaTicketDispenser/src/vending/MultiplexState.java
+++ CinemaTicketDispenser/src/vending/MultiplexState.java
@@ -1,5 +1,32 @@
 package vending;
 
-public class MultiplexState {
+import java.util.ArrayList;
+
+public class MultiplexState implements Serializable{
+    
+    private ArrayList<Theater> theaterList;
+    private MultiplexState instance;
+    
+    private MultiplexState(){
+        this.theaterList = new ArrayList<>();
+    }
+    
+    public MultiplexState getInstance(){
+        if(instance == null)
+            instance = new MultiplexState();
+        return instance;
+    }
+    
+    public Theater getTheater(int i){
+        return theaterList.get(i);
+    }
+    
+    public int getNumberOfTheaters(){
+        return theaterList.size();
+    }
+    
+    public ArrayList<Theater> getTheaterList(){
+        return theaterList;
+    }
     
 }
